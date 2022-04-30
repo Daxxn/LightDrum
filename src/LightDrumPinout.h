21e65d0b17fe415489ec39f6115140d38d1a4f0a
Cody Lantz <46167982+Daxxn@users.noreply.github.com>
2022-04-30 14:49:36 -0700

    REV3 Initial Commit

---
+++ src/LightDrumPinout.h
@@ -0,0 +1,95 @@
+// Auto-Generated code file from PinGenerator.
+// Avoid editing this file.
+
+#pragma once
+#include <Arduino.h>
+
+namespace Digitl {
+	namespace LEDPins {
+		const int SDI_PIN = 36;
+		const int CLK_PIN = 37;
+		const int LE_PIN = 40;
+		const int OE_PIN = 41;
+	}
+	namespace LCDPins {
+		const int D0_PIN = 42;
+		const int D1_PIN = 43;
+		const int D2_PIN = 44;
+		const int D3_PIN = 45;
+		const int D4_PIN = 46;
+		const int D5_PIN = 49;
+		const int D6_PIN = 48;
+		const int D7_PIN = 47;
+		const int RS_PIN = 38;
+		const int RW_PIN = 18;
+		const int EN_PIN = 19;
+	}
+	namespace POWERPins {
+		const int RELAY_PIN = 69;
+	}
+	namespace BUTTONSPins {
+		const int ACT_PIN = 14;
+		const int HOME_PIN = 27;
+		const int M_UP_PIN = 29;
+		const int M_DOWN_PIN = 25;
+		const int MODE_PIN = 24;
+	}
+	namespace ENCODERPins {
+		const int A1_PIN = 4;
+		const int B1_PIN = 59;
+		const int SW1_PIN = 0;
+		const int A2_PIN = 61;
+		const int B2_PIN = 62;
+		const int SW2_PIN = 60;
+	}
+	namespace AUDIOPins {
+		const int XLR_SW_PIN = 34;
+		const int HP_SW_PIN = 31;
+		const int MIDI_PIN = 17;
+	}
+	namespace INDICATORPins {
+		const int M_DOWN_PIN = 22;
+		const int M_UP_PIN = 28;
+		const int HOME_PIN = 26;
+		const int MODE_PIN = 23;
+		const int ACT_PIN = 39;
+	}
+}
+namespace Anlg {
+	namespace CURR_SENSEPins {
+		const int S0_PIN = A13;
+		const int S1_PIN = A11;
+		const int S2_PIN = A4;
+		const int S3_PIN = A2;
+		const int S4_PIN = A12;
+		const int S5_PIN = A10;
+		const int S6_PIN = A3;
+		const int S7_PIN = A1;
+	}
+}
+namespace Ser {
+	namespace SPI {
+		const int SCLK = 52;
+		const int MISO = 50;
+		const int MOSI = 51;
+		const int SS = 53;
+
+		const int SPECT_CS = 68;
+	}
+	namespace I2C {
+		const int SCLK = 21;
+		const int SDA = 20;
+
+		const int PWM_OE = 16;
+
+		const int PWM0_ADDR = 1;
+		const int PWM1_ADDR = 2;
+		const int PWM2_ADDR = 3;
+		const int MEM_ADDR = 0;
+	}
+}
+
+// Author: Daxxn
+// Auto-Generated code file from PinGenerator.
+// Avoid editing this file.
+
